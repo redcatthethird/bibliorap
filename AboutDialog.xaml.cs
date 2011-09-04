5bc29f68ffe19adf12928d0696ead41729f47645
Red Cat <redcat23.com96@gmail.com>
2011-09-04 16:15:29 +0300

    Fixed big mistake (hopefully), added ToolTip for "Thumbnail.er" button, and also started working on a tabbed display. Not nearly there yet. Just to make it clear.

---
+++ AboutDialog.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BiblioRap
+{
+	/// <summary>
+	/// Interaction logic for AboutDialog.xaml
+	/// </summary>
+	public partial class AboutDialog : Window
+	{
+		public AboutDialog()
+		{
+			InitializeComponent();
+		}
+
+		private void AboutDialog_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			this.Title = "Source = " + e.Source.GetType().Name + ", Original Source = " + e.OriginalSource.GetType().Name + " @ " + e.Timestamp;
+
+			Control source = e.Source as Control;
+
+			if (source.BorderThickness != new Thickness(5))
+			{
+				source.BorderThickness = new Thickness(5);
+				source.BorderBrush = Brushes.Black;
+			}
+			else
+				source.BorderThickness = new Thickness(0);
+		}
+
+		private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (e.AddedItems.Count > 0)
+				MessageBox.Show("You just selected " + e.AddedItems[0]);
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("You just clicked " + e.Source);
+		}
+
+		private void HelpCanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void HelpHasExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			MessageBox.Show(this,
+				"Did you really think anyone can help you ? "
+				+ "Hah hah hah hah... you are on your own on this, kiddo'. "
+				+ "Best wishes, Red. a.k.a your worst nightmare...",
+				"Useless Info",
+				MessageBoxButton.OK,
+				MessageBoxImage.Information);
+		}
+
+		private void CloseCanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void CloseHasExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			this.DialogResult = true;
+		}
+	}
+}
---
+++ ThumbnailViewer.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.WindowsAPICodePack.Shell;
+
+namespace BiblioRap
+{
+	/// <summary>
+	/// Interaction logic for TestPage.xaml
+	/// </summary>
+	public partial class ThumbnailViewer : Window
+	{
+		private ListBox refList = null;
+		private int index = -1;
+		private ThumbnailSize thumbSize = ThumbnailSize.ExtraLarge;
+
+		public ThumbnailViewer(ListBox lBox)
+		{
+			refList = lBox;
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			index = refList.SelectedIndex;
+			Refresh();
+			foreach (ThumbnailSize size in Enum.GetValues(typeof(ThumbnailSize)))
+			{
+				DisplayModer.Items.Add(size.ToString());
+			}
+			DisplayModer.SelectedIndex = 0;
+
+			
+		}
+
+		private void Displayer_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			MessageBox.Show("Width = " + Displayer.Source.Width + "| Height = " + Displayer.Source.Height);
+			MessageBox.Show(Lefter.ActualWidth + " | " + Lefter.ActualHeight + "\n"
+				 + Righter.ActualWidth + " | " + Righter.ActualHeight + "\n"
+				 + this.Width + " | " + this.Height + "\n"
+				 + (this.Width - Lefter.ActualWidth - Righter.ActualWidth) + " | "
+				 + (this.Height - DisplayModer.ActualHeight) + "\n"
+				 + Displayer.ActualWidth + " | " + Displayer.ActualHeight);
+		}
+
+		private void Lefter_Click(object sender, RoutedEventArgs e)
+		{
+			index--;
+			Refresh();
+		}
+
+		private void Righter_Click(object sender, RoutedEventArgs e)
+		{
+			index++;
+			Refresh();
+		}
+
+		private void Refresh()
+		{
+			// Overflow behavior.
+			if (index > refList.Items.Count - 1) index = 0;
+			if (index < 0) index = refList.Items.Count - 1;
+
+			FileInfo fileInfo = refList.Items[index] as TFileInfo;
+			if (fileInfo == null)
+				throw new ArgumentNullException(
+					"Something fucked up, the thing selected in the ItemsControl ain't a FileInfo.");
+
+			Displayer.Source = Thumbnail(fileInfo, thumbSize);
+
+			this.Title = fileInfo.Name;
+		}
+
+		BitmapSource Thumbnail(FileInfo fi, ThumbnailSize ts)
+		{
+			switch (ts)
+			{
+				case ThumbnailSize.ExtraLarge:
+					return ShellFile.FromFilePath(fi.FullName).Thumbnail.ExtraLargeBitmapSource;
+				case ThumbnailSize.Large:
+					return ShellFile.FromFilePath(fi.FullName).Thumbnail.LargeBitmapSource;
+				case ThumbnailSize.Medium:
+					return ShellFile.FromFilePath(fi.FullName).Thumbnail.MediumBitmapSource;
+				case ThumbnailSize.Small:
+					return ShellFile.FromFilePath(fi.FullName).Thumbnail.SmallBitmapSource;
+				default:
+					return ShellFile.FromFilePath(fi.FullName).Thumbnail.LargeBitmapSource;
+			}
+		}
+
+		private void DisplayModer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			thumbSize = (ThumbnailSize)Enum.Parse(typeof(ThumbnailSize), e.AddedItems[0] as string);
+			Refresh();
+		}
+	}
+
+	public enum ThumbnailSize
+	{
+		ExtraLarge,
+		Large,
+		Medium,
+		Small
+	}
+}
