feccb52f14e1b1d61014d782c424c524d1853827
Red Cat <redcat23.com96@gmail.com>
2012-05-31 22:53:19 +0300

    Developed a naive partial implementation of a tool designed to help users in removing name duplicate files.

---
+++ Misc.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Interop;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using RMA.Shell;
+using System.Windows.Controls;
+
+namespace BiblioRap
+{
+	public static class Misc
+	{
+		public static bool Windows7;
+
+		static Misc()
+		{
+			Version vs = Environment.OSVersion.Version;
+
+			if (vs.Major == 6 && vs.Minor != 0)
+				Windows7 = true;
+			else
+				Windows7 = false;
+		}
+
+		private static Action EmptyDelegate = delegate() { };
+		public static void Refresh(this UIElement elem)
+		{
+			elem.Dispatcher.Invoke(EmptyDelegate, DispatcherPriority.Render);
+		}
+
+		public static bool IsNullOrEmpty(this string str)
+		{
+			return str == null || str.Length == 0;
+		}
+
+		/// <summary>
+		/// Returns the Windows-displayed thumbnail of a file described by a FileInfo.
+		/// Using the shell. Windows dll. Supposedly working, but not really.
+		/// </summary>
+		/// <param name="fi">FileInfo specifying the file.</param>
+		/// <returns>Thumbnail of the file.</returns>
+		public static Bitmap GetThumbnail(this FileInfo fi)
+		{
+			Bitmap thumb;
+			using (ShellThumbnail st = new ShellThumbnail())
+			{
+				thumb = st.GetThumbnail(fi.FullName);
+			}
+			return thumb;
+		}
+
+		[DllImport("gdi32", CharSet = CharSet.Auto)]
+		internal extern static int DeleteObject(IntPtr hObject);
+
+		public static BitmapSource ToBitmapSource(this Bitmap bmp)
+		{
+			BitmapSource bs;
+			IntPtr ip = bmp.GetHbitmap();
+			try
+			{
+				bs = Imaging.CreateBitmapSourceFromHBitmap(
+					ip,
+					IntPtr.Zero,
+					Int32Rect.Empty,
+					BitmapSizeOptions.FromEmptyOptions());
+			}
+			finally
+			{
+				DeleteObject(ip);
+			}
+
+			return bs;
+		}
+
+		public static T Clamp<T>(this T nr, T min, T max)
+			where T : IComparable<T>
+		{
+			if (nr.CompareTo(min) < 0)
+				return min;
+			if (nr.CompareTo(max) > 0)
+				return max;
+			return nr;
+		}
+
+		public static void Add<T>(this ItemCollection target, IEnumerable<T> source)
+		{
+			foreach (T item in source)
+				target.Add(item);
+		}
+
+		public static BitmapSource BitmapSource(this Bitmap bmp)
+		{
+			return bmp.ToBitmapSource();
+		}
+
+		/*public static Bitmap GetResized(this Bitmap bmp, int width, int height)
+		{
+			MemoryStream ms = new MemoryStream();
+			bmp.Save(ms, ImageFormat.Png);
+			BitmapImage bi = new BitmapImage();
+			bi.BeginInit();
+			if (width > 0)
+				bi.DecodePixelWidth = width;
+			if (height > 0)
+				bi.DecodePixelHeight = height;
+			bi.StreamSource = ms;
+			bi.EndInit();
+
+
+			MemoryStream mse = new MemoryStream();
+			//BmpBitmapEncoder e = new BmpBitmapDecoder();
+		}*/
+	}
+
+	public static class VisualTraverser
+	{
+		public static Child GetFirstChild<Child>(DependencyObject list)
+			where Child : DependencyObject
+		{
+			DependencyObject candidate = null;
+			for (int i = 0; i < VisualTreeHelper.GetChildrenCount(list); i++)
+			{
+				candidate = VisualTreeHelper.GetChild(list, i);
+				if (candidate is Child)
+					break;
+			}
+			if (!(candidate is Child))
+				for (int i = 0; i < VisualTreeHelper.GetChildrenCount(list); i++)
+				{
+					candidate = VisualTreeHelper.GetChild(list, i);
+					candidate = GetFirstChild<Child>(candidate);
+					if (candidate is Child)
+						break;
+				}
+			return candidate as Child;
+		}
+
+		// Important: Here I assume the ListBoxItem contains a single TextBlock.
+		public static string GetItemString(ListBoxItem item)
+		{
+			return GetFirstChild<TextBlock>(item).Text;
+		}
+	}
+}
---
+++ TFileInfo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using Microsoft.WindowsAPICodePack.Shell;
+
+namespace BiblioRap
+{
+	public class TFileInfo
+	{
+		private FileInfo fileInfo;
+
+		public TFileInfo(FileInfo fi)
+		{
+			fileInfo = fi;
+		}
+
+		public String FullName
+		{
+			get { return fileInfo.FullName; }
+		}
+		public String Name
+		{
+			get { return fileInfo.Name; }
+		}
+
+		private bool isPic
+		{
+			get
+			{
+				foreach (string str in MainWindow.PicExt)
+					if (fileInfo.FullName.EndsWith(str))
+						return true;
+				return false;
+			}
+		}
+
+		private BitmapSource _thumbnail;
+		private bool _thIsInit = false;
+		public BitmapSource Thumbnail
+		{
+			get
+			{
+				if (!_thIsInit)
+				{
+					double screenW = System.Windows.SystemParameters.PrimaryScreenWidth;
+					double screenH = System.Windows.SystemParameters.PrimaryScreenHeight;
+
+					if (Misc.Windows7)
+					{
+						//_thumbnail = fileInfo.GetThumbnail().ToBitmapSource();
+						ShellFile sf = ShellFile.FromFilePath(fileInfo.FullName);
+						Bitmap b = sf.Thumbnail.SmallBitmap;
+						bool reswidth = b.Width/screenW > b.Height/screenH; // should I resize the width ?
+						//(int)System.Windows.SystemParameters.PrimaryScreenWidth / 10;
+						_thumbnail = sf.Thumbnail.ExtraLargeBitmap.ToBitmapSource();
+					}
+					else
+					{
+						if (isPic)
+						{
+							_thumbnail = new Bitmap(Image.FromFile(fileInfo.FullName)).ToBitmapSource();
+						}
+						else
+							_thumbnail = Icon.ExtractAssociatedIcon(fileInfo.FullName).ToBitmap().ToBitmapSource();
+					}
+				}
+				return _thumbnail;
+			}
+		}
+
+		public static implicit operator FileInfo(TFileInfo fi)
+		{
+			return new FileInfo(fi.FullName);
+		}
+		public static implicit operator TFileInfo(FileInfo fi)
+		{
+			return new TFileInfo(fi);
+		}
+	}
+}
